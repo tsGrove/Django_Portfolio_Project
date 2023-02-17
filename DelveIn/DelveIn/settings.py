0d5403246418b304568e3a99f156c422cc1e595c
Tyler Hargrove <ts.hargrove95@gmail.com>
2023-02-17 01:55:29 -0600

    Basic Functionality Completed

--- DelveIn/DelveIn/settings.py
+++ DelveIn/DelveIn/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+EMAIL = os.environ.get('EMAIL')
+PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +137,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL KEY')
---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+EMAIL = os.environ.get('EMAIL')
+PASSWORD = os.environ.get('EMAIL_PASSWORD')
+GMAIL = os.environ.get("GMAIL KEY")
+print(EMAIL, PASSWORD, GMAIL)
+
